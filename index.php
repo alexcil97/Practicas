72d23aad7ef7ee1352161041ca4315739bb763d5
Alex_Cil <alejandro.ciljimenez@gmail.com>
2024-03-18 13:18:03 +0100

    Committing local changes before cherry-pick

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 hola
 esto es un cambio para que se refleje
 estos son cambios de rama
-<<<<<<< HEAD
 ultimo cambio del curso 1
-=======
 aqui empieza el segundo curso 
 el segundo cambio 
->>>>>>> 6bcffd1 (este es el incio del segundo curso --modificado)
+esto es de cara al cherry-pick
