09bb482a9614d99cd845bc5b632a28c5a59b052a
Alex_Cil <alejandro.ciljimenez@gmail.com>
2024-03-18 13:01:30 +0100

    este es el incio del segundo curso --modificado

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
+
 hola
 esto es un cambio para que se refleje
 estos son cambios de rama
+<<<<<<< HEAD
 ultimo cambio del curso 1
+=======
+aqui empieza el segundo curso 
+el segundo cambio 
+>>>>>>> 6bcffd1 (este es el incio del segundo curso --modificado)
